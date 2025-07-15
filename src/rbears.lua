bb343797d4ab1781fbe3f91ebd6766196621a745
stronggato <veremeychikplaton@gmail.com>
2025-07-15 15:19:18 +0300

    Update rbears.lua

--- src/rbears.lua
+++ src/rbears.lua
@@ -1,6 +1,6 @@
-local b = game.Workspace.bear
-local b2 = game.Workspace.bear2
-local b3 = game.Workspace.bear3
+local b = game.Workspace.Bear
+local b2 = game.Workspace.Bear2
+local b3 = game.Workspace.Bear3
 
 local function RemoveBears()
   b:Destroy()
